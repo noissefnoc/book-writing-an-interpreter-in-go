38ad68f06489da0cc8ab45e9ee45583d39030c5c
noissefnoc <noissefnoc@gmail.com>
2018-06-18 01:00:55 +0900

    complete implement let statement parser at section 2.4

---
+++ monkey/ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "github.com/noissefnoc/book-writing-an-interpreter-in-go/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT token
+	Value string
+}
+
+func (i *Identifier) experessionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
