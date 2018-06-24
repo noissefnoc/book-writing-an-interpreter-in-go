4e5fa8bbec4a53d1e555a44fe47d3f21b5ad4919
noissefnoc <noissefnoc@gmail.com>
2018-06-25 00:49:05 +0900

    complete implement chapter2

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/noissefnoc/book-writing-an-interpreter-in-go/monkey/lexer"
-	"github.com/noissefnoc/book-writing-an-interpreter-in-go/monkey/token"
+	"github.com/noissefnoc/book-writing-an-interpreter-in-go/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
