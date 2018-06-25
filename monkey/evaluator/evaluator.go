ffa95e15fb79a27526db2ca64006924cef6872fd
noissefnoc <noissefnoc@gmail.com>
2018-06-26 00:00:31 +0900

    add boolean evaluator

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/noissefnoc/book-writing-an-interpreter-in-go/monkey/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// statement
@@ -15,6 +21,8 @@ func Eval(node ast.Node) object.Object {
 	// literal
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -28,4 +36,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
