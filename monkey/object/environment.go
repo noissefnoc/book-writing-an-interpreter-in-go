2b281834e038fbffd4713359c2dcf1475ef4508d
noissefnoc <noissefnoc@gmail.com>
2018-06-28 00:36:42 +0900

    complete implement section 3.10 (function and closure)

--- monkey/object/environment.go
+++ monkey/object/environment.go
@@ -1,5 +1,11 @@
 package object
 
+func NewEncloseEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
 	return &Environment{store: s}
@@ -7,10 +13,14 @@ func NewEnvironment() *Environment {
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
