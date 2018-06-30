b1090e4a8b6a21fc6a3ab494b8a279c5eb4bc220
noissefnoc <noissefnoc@gmail.com>
2018-06-30 23:37:26 +0900

    complete implement A.5.3

--- monkey/object/object.go
+++ monkey/object/object.go
@@ -22,6 +22,7 @@ const (
 	ARRAY_OBJ = "ARRAY"
 	HASH_OBJ = "HASH"
 	QUOTE_OBJ = "QUOTE"
+	MACRO_OBJ = "MACRO"
 )
 
 type Object interface {
@@ -188,3 +189,28 @@ func (q *Quote) Inspect() string {
 	return "QUOTE(" + q.Node.String() + ")"
 }
 func (q *Quote) Type() ObjectType { return QUOTE_OBJ }
+
+type Macro struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (m *Macro) Inspect() string {
+	var out  bytes.Buffer
+
+	params := []string{}
+	for _, p := range m.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("macro")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(m.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (m *Macro) Type() ObjectType { return MACRO_OBJ }
